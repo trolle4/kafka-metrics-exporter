2a905559327866e522c2cbbc6311b2d74da1ccd3
Troels Rosborg Larsen <trb@in-commodities.com>
2024-12-06 19:36:03 +0100

    Adding integration tests using test-containers

--- src/main/java/com/trolle4/kafka/exporter/KafkaExporterApplication.java
+++ src/main/java/com/trolle4/kafka/exporter/KafkaExporterApplication.java
@@ -8,8 +8,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class KafkaExporterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaExporterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaExporterApplication.class, args);
+    }
 
 }
